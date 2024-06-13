09898027c4ff673106c4403a9249161ca5aba2c2
mculha <melih.culha.09@gmail.com>
2024-06-13 23:15:10 +0300

    Initial Commit

---
+++ SwiftUIZoomTransition/SwiftUIZoomTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIZoomTransitionApp.swift
+//  SwiftUIZoomTransition
+//
+//  Created by Melih Çulha on 13.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIZoomTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
